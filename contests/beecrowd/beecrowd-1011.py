991809175e20b7d4367fb23e9be4754e3e1275f9
Leonardo Moraes <leo.mauro.desenv@gmail.com>
2025-09-13 15:49:30 -0300

    Create beecrowd-1011.py

---
+++ contests/beecrowd/beecrowd-1011.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+r = float(input())
+pi = 3.14159
+volume = (4/3)*pi*(r*r*r)
+print(f"VOLUME = {volume:.3f}")
