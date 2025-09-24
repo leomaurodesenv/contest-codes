7710af72026a1466c9559c6f170755d6dc7d25c0
Leonardo Moraes <leo.mauro.desenv@gmail.com>
2025-09-23 22:03:42 -0300

    feat: add beecrowd-1866.py

---
+++ contests/beecrowd/beecrowd-1866.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+n = int(input())
+for i in range(n):
+    v = int(input())
+    print(v%2)
